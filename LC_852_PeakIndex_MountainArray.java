922868709c8416c9ac4d8ff8782a09c474646ecb
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-03-28 17:42:27 +0530

    Update LC_852_PeakIndex_MountainArray.java

--- LC_852_PeakIndex_MountainArray.java
+++ LC_852_PeakIndex_MountainArray.java
@@ -9,8 +9,8 @@ class Solution {
 
         while(start!=end){
             int mid = start + (end - start)/2 ;
-            if (arr[mid]<arr[mid+1]) start=mid+1;
-            else end=mid; 
+            if (arr[mid]<arr[mid+1]) start=mid+1; //you are in increasing part of array hence mid+1 element > mid element
+            else end=mid; //you are in decreasing part so this may be the ans but look at left side with including mid (that's why we haven't used end=mid-1)
         }
         return start;
     }
