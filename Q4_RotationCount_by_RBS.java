23e0d9e27177f66b5ac8b2234fb42b2b4e1fd229
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-06-01 21:57:29 +0530

    Update Q4_RotationCount_by_RBS.java

--- Q4_RotationCount_by_RBS.java
+++ Q4_RotationCount_by_RBS.java
@@ -7,14 +7,14 @@ public class Q4_RotationCount_by_RBS {
         System.out.println(RotationCount(test_arr1, true));
     }
 
-    static int  RotationCount(int[] arr, boolean dup) {
-        if (dup == false){
+    static int  RotationCount(int[] arr, boolean dup) { //
+        if (dup == false){  //false means it's a non duplicate array but NOTE : pivot function for duplicates will run finely for non-duplicate arrays.
             int pivot = findPivot(arr); //doing for non-duplicates now
             if (pivot==-1) return 0;
             else return pivot+1;
         }
         else{
-            int pivot1 = findPivot_duplicate(arr);
+            int pivot1 = findPivot_duplicate(arr); //doing for duplicates
             if (pivot1==-1) return 0;
             else return pivot1+1;
         }
@@ -60,3 +60,7 @@ public class Q4_RotationCount_by_RBS {
         return -1;
     }
 }
+
+//Output :
+// 3
+// 4
